27e2a779b5f669aa2b82b7d53243442220f36c7f
naldurien <nicole.aldurien@gmail.com>
2021-08-08 16:09:32 -0400

    modifying styling

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ export default function App() {
 	return (
 		<div>
 			<div class="transbox">
-				<p><em>Thanks for visiting! <br/>Please select your desired topic below.</em></p>
+				<p><em>Thanks for visiting! <br/>We will be adding additional content over time. To suggest questions or corrections, please email the site administrator at <a href="mailto:sbbreviewsite@gmail.com" class="email-link">sbbreviewsite@gmail.com</a>.</em></p>
+			</div>
+			<div class="transbox">
+				<p><em>Please select your desired topic below:</em></p>
 				<br/>
 				<br/>
 				<NavLink class="nav-links" to="/ABO">1) A, B, O, and H Blood Groups</NavLink>
-				<NavLink class="nav-links" to="/HLA">2) Human Leukocyte Antigens</NavLink>
+				<NavLink class="nav-links" to="/HLA">2) Human Leukocyte Antigens (HLA)</NavLink>
 			</div>
 		</div>
 	);
--- src/HLA.js
+++ src/HLA.js
@@ -5,94 +5,104 @@ export default function HLA() {
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
 	const questions = [
-		{
-			questionText: 'Which immunodominant sugar is responsible for the H antigen?',
+        {
+			questionText: 'Which class of HLA molecules present exogenous/extracellular peptides?',
 			answerOptions: [
-				{ answerText: 'N-acetyl-D-galactosamine', isCorrect: false },
-				{ answerText: 'D-galactose', isCorrect: false },
-				{ answerText: 'L-fucose', isCorrect: true },
+				{ answerText: 'HLA Class I', isCorrect: false },
+				{ answerText: 'HLA Class II', isCorrect: true },
+				{ answerText: 'HLA Class III', isCorrect: false },
+				{ answerText: 'None of the above', isCorrect: false },
+			],
+		},
+        {
+			questionText: 'HLA Class II molecules are recognized by which type of T cell?',
+			answerOptions: [
+				{ answerText: 'CD4 T-helper lymphocytes', isCorrect: true },
+				{ answerText: 'CD56 Natural Killer (NK cells)', isCorrect: false },
+				{ answerText: 'CD8 cytotoxic T lymphocytes', isCorrect: false },
+				{ answerText: 'CD20 memory B cells', isCorrect: false },
+			],
+		},
+        {
+			questionText: 'Which HLA class is found only on B-lymphocytes, macrophages, dendritic cells, and activated T cells?":',
+			answerOptions: [
+				{ answerText: 'HLA Class I', isCorrect: false },
+				{ answerText: 'HLA Class II', isCorrect: true },
+				{ answerText: 'HLA Class III', isCorrect: false },
+				{ answerText: 'None of the above', isCorrect: false },
+			],
+		},
+        {
+			questionText: 'In which of the following clinical conditions is HLA *least* important as a consideration?',
+			answerOptions: [
+				{ answerText: 'Platelet refractoriness', isCorrect: false },
+				{ answerText: 'Alloimmunization in sickle cell disease', isCorrect: true },
+				{ answerText: 'Transfusion-associated GvHD', isCorrect: false },
+				{ answerText: 'TRALI', isCorrect: false },
+			],
+		},
+        {
+			questionText: 'Which HLA class is found on platelets and almost all nucleated cells in the body?',
+			answerOptions: [
+				{ answerText: 'HLA Class I', isCorrect: true },
+				{ answerText: 'HLA Class II', isCorrect: false },
+				{ answerText: 'HLA Class III', isCorrect: false },
 				{ answerText: 'None of the above', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'Which of the following is NOT associated with acquired B antigen?',
+			questionText: 'ABO compatibility is more important than HLA compatibility for which types of transplants?',
 			answerOptions: [
-				{ answerText: 'Carcinoma of the colon or rectum', isCorrect: false },
-				{ answerText: 'Mycobacteria pneumoniae', isCorrect: true },
-				{ answerText: 'Gram-negative bacteria', isCorrect: false },
-				{ answerText: 'Intestinal obstructions', isCorrect: false },
+				{ answerText: 'Liver and lung', isCorrect: false },
+				{ answerText: 'Corneal and ligament', isCorrect: false },
+				{ answerText: 'Heart and kidney', isCorrect: true },
+				{ answerText: 'Hemaopoetic stem cell', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'ABO antigens are sensitive to which of the following chemical treatments?',
+			questionText: 'Which of the following is a Class II HLA gene?',
 			answerOptions: [
-				{ answerText: 'Ficin', isCorrect: false },
-				{ answerText: 'Trypsin', isCorrect: false },
-				{ answerText: 'DTT', isCorrect: false },
-				{ answerText: 'None of the above', isCorrect: true },
+				{ answerText: 'C4b', isCorrect: false },
+				{ answerText: 'Complement C2', isCorrect: false },
+				{ answerText: 'HLA-A', isCorrect: false },
+				{ answerText: 'HLA-DQ', isCorrect: true },
 			],
 		},
 		{
-			questionText: 'Which of the following is NOT a possible cause of mixed-field agglutination?',
+			questionText: 'HLA Class I molecules are recognized by which type of T cell?',
 			answerOptions: [
-				{ answerText: 'Depressed immunoglobulin levels in immunodeficient patients', isCorrect: true },
-				{ answerText: 'Recent transfusion', isCorrect: false },
-				{ answerText: 'Bone marrow transplant recipient', isCorrect: false },
-				{ answerText: 'A3 subgroup', isCorrect: false },
+				{ answerText: 'CD4 T-helper lymphocytes', isCorrect: false },
+				{ answerText: 'CD56 Natural Killer (NK cells)', isCorrect: false },
+				{ answerText: 'CD8 cytotoxic T lymphocytes', isCorrect: true },
+				{ answerText: 'CD20 memory B cells', isCorrect: false },
+			],
+		},
+
+		{
+			questionText: 'Which 2 complement proteins are also HLA class III proteins and RBC antigens?',
+			answerOptions: [
+				{ answerText: 'C1q and C1r', isCorrect: false },
+				{ answerText: 'C3b and C3d', isCorrect: false },
+				{ answerText: 'C4b and C4a', isCorrect: true },
+				{ answerText: 'C8 and C9', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'E. coli O86 is the heterophile antigen that causes us to make "naturally occurring":',
+			questionText: 'Which class of HLA molecules present endogenous peptides?',
 			answerOptions: [
-				{ answerText: 'Anti-A', isCorrect: false },
-				{ answerText: 'Anti-B', isCorrect: true },
-				{ answerText: 'Anti-A,B', isCorrect: false },
-				{ answerText: 'Anti-M', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How would you resolve rouleaux interference in an ABO type?',
-			answerOptions: [
-				{ answerText: '15-minute room temp incubation', isCorrect: false },
-				{ answerText: 'Ficin treatment', isCorrect: false },
-				{ answerText: 'Saline replacement', isCorrect: true },
-				{ answerText: 'DTT treatment', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'What percentage of the population are secretors?',
-			answerOptions: [
-				{ answerText: '20%', isCorrect: false },
-				{ answerText: '40%', isCorrect: false },
-				{ answerText: '60%', isCorrect: false },
-				{ answerText: '80%', isCorrect: true },
-			],
-		},
-		{
-			questionText: 'Anti-A1 is considered clinically insignificant unless it reacts at:',
-			answerOptions: [
-				{ answerText: '4C', isCorrect: false },
-				{ answerText: '20C', isCorrect: false },
-				{ answerText: '37C', isCorrect: true },
+				{ answerText: 'HLA Class I', isCorrect: true },
+				{ answerText: 'HLA Class II', isCorrect: false },
+				{ answerText: 'HLA Class III', isCorrect: false },
 				{ answerText: 'None of the above', isCorrect: false },
 			],
 		},
-		{
-			questionText: 'What test serologically distinguishes A1 from other subgroups of A?',
+        {
+			questionText: 'How would serologic lymphocytotoxicity cell death score of 18% be interpreted?',
 			answerOptions: [
-				{ answerText: 'Dolichos biflorus', isCorrect: true },
-				{ answerText: 'Ulex europaeus', isCorrect: false },
-				{ answerText: 'Vicea graminea', isCorrect: false },
-				{ answerText: 'Griffonia simplicifolia', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Acquired B antigen is a phenomenon that can occasionally be seen in individuals with blood group:',
-			answerOptions: [
-				{ answerText: 'O', isCorrect: false },
-				{ answerText: 'A', isCorrect: true },
-				{ answerText: 'Both O and A', isCorrect: false },
-				{ answerText: 'None of the above', isCorrect: false },
+				{ answerText: 'Negative', isCorrect: false },
+				{ answerText: 'Doubtful positive', isCorrect: false },
+				{ answerText: 'Weak positive', isCorrect: true },
+				{ answerText: 'Positive', isCorrect: false },
 			],
 		},
 	];
